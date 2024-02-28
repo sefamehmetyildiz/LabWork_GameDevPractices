5edd8538b4809035f24899d708c309900b0660c2
Sefa Mehmet YILDIZ <y.sefamehmet@gmail.com>
2024-02-28 14:40:01 +0300

    I created Hello World script.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("FrameTree",3f);
+    }
+    void FrameTree()
+    {
+        Debug.Log("Hello World");
+        transform.position = new Vector3(10,10,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
